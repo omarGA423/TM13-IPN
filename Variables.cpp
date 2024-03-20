eacfc6cb4fb356f9a9331908d8a6e4ee337e7ab5
omarGA423 <161617963+omarGA423@users.noreply.github.com>
2024-03-20 16:09:53 -0600

    Delete Variables.cpp

--- Variables.cpp
+++
@@ -1,7 +0,0 @@
-﻿Operación Compilar iniciada..
-1>------ Operación Compilar iniciada: proyecto: Variables.cpp, configuración: Debug x64 ------
-1>Variables.cpp.cpp
-1>C:\Users\USUARIO\Desktop\JAZMÍN -65\LPRUEVA\Variables.cpp\Variables.cpp\Variables.cpp.cpp(9,16): warning C4305: 'inicializando': truncamiento de 'double' a 'float'
-1>Variables.cpp.vcxproj -> C:\Users\USUARIO\Desktop\JAZMÍN -65\LPRUEVA\Variables.cpp\x64\Debug\Variables.cpp.exe
-1>Compilación del proyecto "Variables.cpp.vcxproj" terminada.
-========== Compilar: 1 correctos, 0 incorrectos, 0 actualizados, 0 omitidos ==========
